a8d7346f8045b735c5f38aaef8a22ef6844ddeac
Mathias Weyland <mathias@weyland.ch>
2013-03-18 18:57:25 +0100

    Friend the IR_test class and make get_orientation private again.

--- code/IR.h
+++ code/IR.h
@@ -4,15 +4,17 @@
 #include "config.h"
 
 enum orientation {IR_WEST, IR_SOUTH, IR_EAST, IR_NORTH, IR_UNKNOWN};
+class IR_test;
 
 class IR
 {
   public:
     void begin();
     double get_avg_angle();
-    orientation get_orientation();
+    friend class IR_test;
 
   private:
+    orientation get_orientation();
     unsigned short window[IR_WINDOW_SIZE];
     unsigned int window_index;
 };
