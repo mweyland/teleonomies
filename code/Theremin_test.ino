061e5f3d03b4672f0d146871e9f0bdb166d3535a
Mathias Weyland <mathias@weyland.ch>
2013-03-20 21:10:08 +0100

    PID (actually just P) for sound output => smooth transitionswq

--- code/Theremin_test.ino
+++ code/Theremin_test.ino
@@ -1,8 +1,12 @@
 #include "Theremin_test.h"
 
+
+/* You can use this together with ArduinoPlot to find
+ * suitable values, e.g. for the map() function below.
+ * https://github.com/gregpinero/ArduinoPlot
+ */
 void Theremin_test::raw_test(Theremin theremin)
 {
-  uint32_t frequency = 0;
   while(!Serial.available())
   {
     while(!theremin.available());
@@ -13,17 +17,20 @@ void Theremin_test::raw_test(Theremin theremin)
 
 void Theremin_test::sound_test(Theremin theremin)
 {
-  uint32_t frequency = 0;
+  uint32_t set_frequency = 0;
+  uint32_t current_frequency = 10;
   while(!Serial.available())
   {
     while(!theremin.available())
     {
-      delayMicroseconds(frequency);
+      delayMicroseconds(current_frequency);
       digitalWrite(SPEAKER_PIN, HIGH);
-      delayMicroseconds(frequency);
+      delayMicroseconds(current_frequency);
       digitalWrite(SPEAKER_PIN, LOW);
+      if(current_frequency < set_frequency) current_frequency += 1;
+      if(current_frequency > set_frequency) current_frequency -= 1;
     }
-    frequency = map(theremin.read(), 270000, 280000, 500, 1000);
+    set_frequency = map(theremin.read(), 270000, 280000, 500, 1000);
   }
   while(Serial.available()) Serial.read();
 }
--- code/teleonomies.ino
+++ code/teleonomies.ino
@@ -53,7 +53,7 @@ asm volatile("jmp 0x7000");
 
 void loop()
 {
-  Theremin_test::raw_test(theremin);
+  Theremin_test::sound_test(theremin);
   Serial.println("Waiting for reset.");
   for(int i=5; i>=0; i--)
   {
