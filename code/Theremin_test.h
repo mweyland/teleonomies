1c6447d0a0d25bdbc664faceebbbd79b51dfc85f
Mathias Weyland <mathias@weyland.ch>
2013-03-27 22:38:31 +0100

    Adding LED effect to Theremin test

--- code/Theremin_test.h
+++ code/Theremin_test.h
@@ -2,12 +2,13 @@
 #define _THEREMIN_TEST_H
 
 #include "Theremin.h"
+#include "LED.h"
 
 class Theremin_test
 {
   public:
     static void raw_test(Theremin theremin);
-    static void sound_test(Theremin theremin);
+    static void sound_test(Theremin theremin, LED led);
 };
 
 #endif
--- code/Theremin_test.ino
+++ code/Theremin_test.ino
@@ -15,7 +15,7 @@ void Theremin_test::raw_test(Theremin theremin)
   while(Serial.available()) Serial.read();
 }
 
-void Theremin_test::sound_test(Theremin theremin)
+void Theremin_test::sound_test(Theremin theremin, LED led)
 {
   uint32_t set_frequency = 0;
   uint32_t current_frequency = 10;
@@ -30,7 +30,14 @@ void Theremin_test::sound_test(Theremin theremin)
       if(current_frequency < set_frequency) current_frequency += 1;
       if(current_frequency > set_frequency) current_frequency -= 1;
     }
-    set_frequency = map(theremin.read(), 270000, 280000, 500, 1000);
+    uint32_t measurment = theremin.read();
+    Serial.print(measurment); Serial.print("\t");
+    set_frequency = map(measurment, 265000, 275000, 500, 1000);
+    Serial.print(set_frequency); Serial.print("\t");
+
+    unsigned char colour = map(measurment, 265000, 275000, 0, 255);
+    Serial.println(colour);
+    led.set_colour(0, 0, colour);
   }
   while(Serial.available()) Serial.read();
 }
